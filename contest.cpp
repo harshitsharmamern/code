484a4e5c1a81f669a585d205e075f81d45ef2fb3
harshit_sharma <sharmaharshit769@gmail.com>
2024-10-05 16:54:10 +0530

    mastar add class

--- contest.cpp
+++ contest.cpp
@@ -1,11 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class cycle{
+    int a;
+    cycle(a){
+        a=a;
+    }
+    void speed(){cout<<a;}
+}
 void fun(int n){
    int x=0;
    for(int i=0;i<5;i++){
     x++;
    }
+
+
+   //////
+   cycle c(5);
+
+
+   ////
+
+
+
+
+//    addcycle
+   
    
 }
 
