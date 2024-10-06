83c165774966bf175e5d4395baed42cd6ef12b02
harshit_sharma <sharmaharshit769@gmail.com>
2024-10-06 19:41:22 +0530

    change code in master

--- contest.cpp
+++ contest.cpp
@@ -2,28 +2,17 @@
 using namespace std;
 
 class cycle{
-    int a;
-    cycle(a){
-        a=a;
+    int val;
+    cycle(int a){
+        val=a;
     }
-    void speed(){cout<<a;}
+    void speed(){
+        cout<<val;
+    }
 }
 void fun(int n){
-   int x=0;
-   for(int i=0;i<5;i++){
-    x++;
-   }
-
-
-   //////
-   cycle c(5);
-
-   cycle c1(6);
-   c1.speed();
-
-   cycle c2(7);cycle c3(8);cycle c4(9);
-   
-   c3.speed();
+    cycle a(5);
+    a.speed();
 }
 
 int main() {
